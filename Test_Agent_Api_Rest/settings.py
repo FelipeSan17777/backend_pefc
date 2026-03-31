9d4b0722691df82d219ae31c3711ae3cfa7f4699
Felipe Andrés Santander Barría <felipe.santander13@inacapmail.cl>
2026-03-31 14:38:33 -0300

    Update settings.py

--- Test_Agent_Api_Rest/settings.py
+++ Test_Agent_Api_Rest/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "Agent_App.apps.AgentAppConfig",
 
     "rest_framework",
     "corsheaders",
