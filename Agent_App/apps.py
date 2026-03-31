c880b9f93d252449e20061a2873f95d582b936f3
Felipe Andrés Santander Barría <felipe.santander13@inacapmail.cl>
2026-03-31 14:34:31 -0300

    Update apps.py

--- Agent_App/apps.py
+++ Agent_App/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class AgentAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Agent_App'
+    def ready(self):
+        import tu_app.signals
